1dc69fa0bb902ac33eff6406394423f875378b4f
Krishnarjun J <165027529+Krishnarjunj@users.noreply.github.com>
2025-05-24 21:44:02 +0530

    Create HashLongSubArrSumK.cpp

---
+++ Arrays/HashLongSubArrSumK.cpp
@@ -0,0 +1,32 @@
+//Krishhhhh
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n, k;
+	cin>>n>>k;
+	vector<ll> a(n);
+	map<ll , ll> hash;
+	for(ll i = 0 ; i < n ; i++){
+		cin>>a[i];
+	}
+	ll sum = 0;
+	ll maxLen = 0;
+
+	for(ll i = 0 ; i < n ; i++){
+		sum+=a[i];
+		hash[sum] = i;
+		if(sum==k){
+			maxLen = max(maxLen , i+1);
+		}
+		ll rem = sum - k;
+		if(hash.find(rem) != hash.end()){
+			maxLen = max(maxLen , i - hash[rem]);
+		}
+	}
+
+	cout<< maxLen;
+
+}
