c36a671536e2b03b712595039f52cb6cb9dcb098
Krishnarjun J <165027529+Krishnarjunj@users.noreply.github.com>
2025-06-17 15:38:45 +0530

    Update 3Sum.cpp

--- Arrays/3Sum.cpp
+++ Arrays/3Sum.cpp
@@ -20,3 +20,57 @@ public:
     }
 };
 
+// OPTIMIZED SOLUTION
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        set<vector<int>> s;
+        for(int i = 0 ; i < n ; i++){
+            set<int> hash;
+            for(int j = i + 1;  j < n ; j++){
+                int third = -(nums[i] + nums[j]);
+                if(hash.find(third)!=hash.end()){
+                    vector<int> temp = {third, nums[i], nums[j]};
+                    sort(temp.begin(), temp.end());
+                    s.insert(temp);
+                }
+                hash.insert(nums[j]);
+            }
+        }
+        vector<vector<int>> ans(s.begin(), s.end());
+        return ans;
+    }
+};
+
+// OPTIMAL SOLUTION
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for(int i = 0; i < n ; i++){
+            if (i!=0 && nums[i]==nums[i-1]) continue;
+            int j = i + 1;
+            int k = n - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum > 0){
+                    k--;
+                }
+                else if(sum < 0){
+                    j++;
+                }
+                else{
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while( j < k && nums[j]==nums[j-1]) j++;
+                    while(j < k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
