b563c2de22b91b23dd7d2316c23bd28c72382269
Krishnarjun J <165027529+Krishnarjunj@users.noreply.github.com>
2025-07-05 09:39:48 +0530

    Update 1_Syntax.cpp

--- Linked List/1_Syntax.cpp
+++ Linked List/1_Syntax.cpp
@@ -1,33 +1,18 @@
-//Krishhhhh
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
 
 class Node{
-public:
+	public:
 	int data;
 	Node* next;
-public:
+
+	public:
 	Node(int data1, Node* next1){
 		data = data1;
 		next = next1;
 	}
-
 	Node(int data1){
 		data = data1;
-		next = NULL;
+		next = nullptr;
 	}
-};
-
-
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	vector<int> arr = {1,2,3,4,5};
-	Node* y = new Node(arr[2]);
-	cout<<y->data;
-
-	
-
 }
