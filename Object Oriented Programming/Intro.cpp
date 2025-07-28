1dfad1d2e7ec6fb5d6899907ba49de165da16170
Krishnarjun J <165027529+Krishnarjunj@users.noreply.github.com>
2025-07-28 22:56:09 +0530

    Create Intro.cpp

---
+++ Object Oriented Programming/Intro.cpp
@@ -0,0 +1,16 @@
+OOPs - #1
+
+CLASSES - Give the blueprint to build objects.
+OBJECTS - Are the entities.
+
+eg : Car Factory - Is a CLASS containing the blueprint to build cars which are the objects
+
+Syntax:
+class Name{
+    //properties
+
+    // Methods
+};
+
+
+
